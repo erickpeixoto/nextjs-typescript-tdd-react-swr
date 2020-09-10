7cdf86ab18ac3bf2cad46f0aa01cad0ae999c55b
Erick Eduardo <erickepeixoto@gmail.com>
2020-09-10 20:27:29 -0300

    chore: add eslint lintstaged husky prettier

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App - CRUD</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Main</main>
+
+      <footer>Footer</footer>
+    </div>
+  )
+}
+export default Home
