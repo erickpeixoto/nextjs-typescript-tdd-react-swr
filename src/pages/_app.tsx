3db41cb553ec63a7d668b7166180348669949bb5
Erick Eduardo <erickepeixoto@gmail.com>
2020-09-11 09:03:05 -0300

    feat: add ThemeProvider and GlobalStyle about Application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from '@style/global'
+import Theme from '@style/theme'
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={Theme}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  )
 }
 export default App
