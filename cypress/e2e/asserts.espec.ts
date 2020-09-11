157de29176d17da76c79630a6f7f841160c70375
Erick Eduardo <erickepeixoto@gmail.com>
2020-09-10 23:55:41 -0300

    chore: add Styled Components and Material Ui Core

---
+++ cypress/e2e/asserts.espec.ts
@@ -0,0 +1,5 @@
+/// <reference types="cypress" />
+it('Equality', () => {
+  const a = 1
+  expect(a).equal(1)
+})
---
+++ src/styles/pages/Main.ts
@@ -0,0 +1,5 @@
+import Styled from 'styled-components'
+
+export const Layout = Styled.div`
+  background: green
+`
