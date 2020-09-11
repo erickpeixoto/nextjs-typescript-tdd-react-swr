dee52a804083bd00cdb9a9095d84c2521937ca93
Erick Eduardo <erickepeixoto@gmail.com>
2020-09-11 18:21:45 -0300

    style: to define grid-template-areas

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import SideBar from '@components/SideBar'
-import { Container, Content } from './styles'
+// import SideBar from '@components/SideBar'
+import { Container, SideBar, NavBar, Content } from './styles'
 const Layout: React.FC = props => {
   return (
     <Container>
-      <SideBar />
+      <NavBar>NB</NavBar>
+      <SideBar>SB</SideBar>
       <Content>{props.children}</Content>
     </Container>
   )
--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -1,7 +1,40 @@
 import Styled from 'styled-components'
 import { Button as useButton } from '@lib/Wrapper'
 
-export const Container = Styled.div``
-export const Content = Styled.div``
 export const Button = Styled(useButton)`
+`
+
+export const Container = Styled.div`
+    display: grid;
+    grid-template-areas: 
+    'NB NB NB'
+    'SB CB CB'; 
+    width: 100vw;
+    height:100vh;
+    background: green;
+     
+    grid-template-rows: 75px calc(100vh - 75px) auto; /* Height */
+    grid-template-columns: ${(props: any) =>
+      props.sidebar ? '80px auto auto' : '250px auto auto'};
+    transition:  grid-template-columns 600ms ease-in-out;
+`
+export const NavBar = Styled.div`
+  grid-area: NB;
+  display: flex;
+  background: yellow;
+`
+
+export const SideBar = Styled.div`
+  grid-area: SB;
+  display: flex;
+  background: red;
+
+`
+
+export const Content = Styled.div`
+  grid-area: CB;
+  display: flex;
+  background: purple;
+  padding: 0px 15px;
+
 `
