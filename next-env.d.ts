5b1fa35d8757d454b9734a316402f3e96908ed41
Erick Eduardo <erickepeixoto@gmail.com>
2020-09-13 14:28:25 -0300

    chore: add enviroment variable endpoints user and to declare types env

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,16 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      AUTH_TOKEN: string
+      API_HOST: string
+      NODE_ENV: 'development' | 'production'
+      PORT?: string
+    }
+  }
+}
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {}
